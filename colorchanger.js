42f28ae814b4f26e830c664d9beaddbfc725c879
Anoop Reddy <anoop.reddy50@gmail.com>
2023-02-15 18:57:37 -0500

    color changer 🌈

--- colorchanger.js
+++ colorchanger.js
@@ -1,62 +1,106 @@
 function switchWhite() {
   document.body.style.backgroundColor = 'white';
   document.body.style.color = 'black';
-  blackButton.style.borderColor = 'black';
-  grayButton.style.borderColor = 'black';
-  redButton.style.borderColor = 'black';
-  greenButton.style.borderColor = 'black';
-  purpleButton.style.borderColor = 'black';
-  whiteButton.style.borderColor = 'black';
-  blueButton.style.borderColor = 'black';
+  blackButton.style['border-color'] = 'black';
+  grayButton.style['border-color'] = 'black';
+  redButton.style['border-color'] = 'black';
+  greenButton.style['border-color'] = 'black';
+  purpleButton.style['border-color'] = 'black';
+  whiteButton.style['border-color'] = 'black';
+  blueButton.style['border-color'] = 'black';
+  const links = document.querySelectorAll('a');
+  links.forEach(link => {
+    link.style.color = 'black';
+  })
 }
 
 function switchBlack() {
   document.body.style.backgroundColor = 'black';
   document.body.style.color = 'white';
-  blackButton.style.borderColor = 'white';
-  grayButton.style.borderColor = 'white';
-  redButton.style.borderColor = 'white';
-  greenButton.style.borderColor = 'white';
-  purpleButton.style.borderColor = 'white';
-  whiteButton.style.borderColor = 'white';
-  blueButton.style.borderColor = 'white';
-}
-
-function switchGray() {
-  document.body.style.backgroundColor = 'gray';
-  document.body.style.color = 'white';
-}
-
-function switchRed(){
-  document.body.style.backgroundColor = 'red';
-  document.body.style.color = 'white';
-}
-
-function switchBlue(){
-  document.body.style.backgroundColor = 'blue';
-  document.body.style.color = 'white';
-}
-
-function switchGreen(){
-  document.body.style.backgroundColor = 'green';
-  document.body.style.color = 'white';
-}
-
-function switchPurple(){
-  document.body.style.backgroundColor = 'purple';
-  document.body.style.color = 'white';
-}
-
-function switchBlue() {
-  document.body.style.backgroundColor = 'blue';
-  document.body.style.color = 'white';
-
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
   const links = document.querySelectorAll('a');
   links.forEach(link => {
     link.style.color = 'white';
   });
 }
 
+function switchGray() {
+  document.body.style.backgroundColor = 'gray';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
+
+function switchRed(){
+  document.body.style.backgroundColor = 'red';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
+
+function switchBlue(){
+  document.body.style.backgroundColor = 'blue';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
+
+function switchGreen(){
+  document.body.style.backgroundColor = 'green';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
+
+function switchPurple(){
+  document.body.style.backgroundColor = 'purple';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
+
+function switchBlue() {
+  document.body.style.backgroundColor = 'blue';
+  document.body.style.color = 'white';
+  blackButton.style['border-color'] = 'white';
+  grayButton.style['border-color'] = 'white';
+  redButton.style['border-color'] = 'white';
+  greenButton.style['border-color'] = 'white';
+  purpleButton.style['border-color'] = 'white';
+  whiteButton.style['border-color'] = 'white';
+  blueButton.style['border-color'] = 'white';
+}
 
 document.getElementById('whiteButton').onclick = switchWhite;
 document.getElementById('blackButton').onclick = switchBlack;
